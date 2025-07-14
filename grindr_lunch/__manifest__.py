d1b02b7a953d479e0c3166d09dd2bafffd427daa
Jaishyam (JRBR) <jrbr@odoo.com>
2025-07-14 15:20:39 -0400

    [FIX]: modifed the manifest to have correct dependencies and data

--- grindr_lunch/__manifest__.py
+++ grindr_lunch/__manifest__.py
@@ -11,13 +11,12 @@
     'category': 'Custom Development',
     'version': '1.0.0',
     'license': 'OPL-1',
-    'depends': ['lunch', 'survey', 'hr', 'product'],
+    'depends': ['lunch', 'survey', 'hr'],
     'data': [
         "views/survey_survey_views.xml",
         'security/ir.model.access.csv',
         'data/ir_cron.xml',
         'data/res_partner.xml',
-        'views/survey_form.xml',
         'views/survey_wizard_form.xml'
     ],
 }
