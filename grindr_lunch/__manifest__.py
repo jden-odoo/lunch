af57fc04ef10f1970cd20e7fba20cb9daf3ead09
jden <jden@odoo.com>
2025-07-14 09:46:45 -0400

    [ADD]  grindr lunch

---
+++ grindr_lunch/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Grindr Lunch',
+    'summary': 'An apple a day keeps the doctor away',
+    'description':
+    """
+        developed by: jrbr-odoo, erga-odoo, jden-odoo
+    """,
+    'author': 'Odoo Development Services',
+    'maintainer': 'Odoo Development Services',
+    'website': 'https://www.odoo.com',
+    'category': 'Custom Development',
+    'version': '1..0.0',
+    'license': 'OPL-1',
+    'depends': ['lunch'],
+    'data': [],
+}
