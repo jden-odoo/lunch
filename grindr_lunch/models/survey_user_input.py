ba654a1c8b38ff0439b011017db618200aeda73c
Erwan <erga@odoo.com>
2025-07-14 16:44:59 -0400

    populating question_ids on survey

--- grindr_lunch/models/survey_user_input.py
+++ grindr_lunch/models/survey_user_input.py
@@ -6,9 +6,13 @@ class SurveyUserInput(models.Model):
     _inherit = 'survey.user_input'
 
     def create(self,vals_list):
-        # Unlink existing answers for the same survey and email
-        self.env['survey.user_input'].search( 
-            ["&", ("survey_id", "=", vals_list[0].get('survey_id')), ("email", "=", vals_list[0].get('email'))]
-        ).unlink()
+        if isinstance(vals_list, list):
+            for val in vals_list:
+                self.env['survey.user_input'].search(
+                    ["&", ("survey_id", "=", vals_list[val].get('survey_id')), ("email", "=", vals_list[val].get('email'))]
+                ).unlink()
+        else:
+            self.env['survey.user_input'].search(
+                ["&", ("survey_id", "=", vals_list.get('survey_id')), ("email", "=", vals_list.get('email'))]
+            ).unlink()
         return super(SurveyUserInput, self).create(vals_list)
-        
